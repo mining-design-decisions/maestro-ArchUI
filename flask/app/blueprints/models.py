49c74c8a7a6e85d172283718755305beb668d0bc
Sarah Druyts <sarah.druyts@gmail.com>
2022-11-18 13:44:56 +0100

    Create New Model
    including various help pages for complicated parameters

--- flask/app/blueprints/models.py
+++ flask/app/blueprints/models.py
@@ -1,6 +1,11 @@
 from flask import render_template
 from flask import Blueprint
-from app.ml_link import get_cli_json
+# from app.ml_link import get_cli_json, get_models_strlist, get_hyper_param_options
+import app.ml_link as lib
+from flask import request
+from flask import redirect
+from flask import url_for
+import json
 
 bp = Blueprint("models", __name__, url_prefix="/models")
 
@@ -15,10 +20,96 @@ def viewall():
 
 @bp.route('/create', methods=["GET"])
 def createModel():
-    options = get_cli_json()
-    print(options)
-    return render_template("models/create.html")
+    options = lib.get_cli_json()
+
+    run_args = None
+    make_features_args = None
+    for cmd in options['commands']:
+        if cmd['name'] == "run":
+            run_args = cmd['args']
+        if cmd['name'] == 'make-features':
+            make_features_args = cmd['args']
+
+    if run_args is None or make_features_args is None:
+        return render_template('error.html')
+
+    # append make-features args
+    run_args.extend(make_features_args)
+
+    to_ignore = [
+        'test-study', # no need in this GUI
+        'store-model', # will be on by default
+        'target-model-path', # will be set JIT
+        'file', # will be set JIT
+        'peregrine', # not in scope
+        'save-trained-generator', # will probably override anyway
+        'ontology-classes', # preset. maybe change later to allow editing. for now set JIT
+    ]
+
+    args = []
+    for arg in run_args:
+        if arg['name'] in to_ignore:
+            continue
+
+        if arg['name'] in ['stacking-meta-classifier','classifier']:
+            arg['type'] = 'enum'
+            arg['options'] = lib.get_models_strlist()
+            arg['help'] = arg['help'].split('.')[0] + '.'
+
+        if arg['name'] == 'input-mode':
+            arg['type'] = 'enum'
+            arg['options'] = lib.get_input_modes()
+            arg['help'] = arg['help'].split('.')[0] + '.'
+        
+        if arg['name'] == 'output-mode':
+            arg['type'] = 'enum'
+            arg['options'] = lib.get_output_modes()
+            arg['help'] = arg['help'].split('.')[0] + '.'
+
+        if arg['name'] == 'params':
+            arg['help'] += " Please find the Parameter Help page in the navbar to correctly format this section."
+
+        if arg['name'] == 'combination-strategy':
+            arg['help'] = arg['help'].split('.')[0] + ". Please find the Combination Strategy Help page in the navbar for more information about this section."
+
+        if 'hyper-param' in arg['name']:
+            arg['help'] += ' Please find the Hyper Parameter Help page in the navbar to correctly format this section.'
+
+        args.append(arg)
+
+    return render_template("models/create.html", args=args)
 
 @bp.route('/create', methods=["POST"])
-def postModel(param):
-    print(param)
+def postModel():
+    # print(request.form)
+    for element in request.form:
+        print(f"{element}: {request.form[element]}")
+
+    # todo validity checking
+    # - including does this name already exist?
+
+    model_obj = {}
+    for element in request.form:
+        if element != "modelname":
+            model_obj[element] = request.form[element]
+    
+    # save
+    with open(f'app/models/{request.form["modelname"]}.json', 'w') as f:
+        json.dump(model_obj, f)
+
+    return redirect(url_for('models.viewall'))
+
+@bp.route('/hyperparams', methods=['GET'])
+def hyperParamHelp():
+    options = lib.get_hyper_param_options()
+    return render_template('models/hyperhelp.html', options=options)
+
+@bp.route('/params', methods=['GET'])
+def paramHelp():
+    options = lib.get_input_mode_params()
+    return render_template('models/paramhelp.html', inmodes=options)
+
+@bp.route('/combination-strategies', methods=['GET'])
+def comboStrat():
+    strats = lib.get_combination_strategies()
+    return render_template('models/comboStrats.html', strats=strats)
--- flask/app/ml_link.py
+++ flask/app/ml_link.py
@@ -1,11 +1,14 @@
 import json
+import os
 from os import path
 import sys
 
-ml_path = path.abspath('../../mining-design-decisions/deep_learning')
+ml_path = path.abspath('../../mining-design-decisions')
 sys.path.append(ml_path)
 
-from dl_manager import cli
+from deep_learning.dl_manager import cli
+from deep_learning.dl_manager import feature_generators
+from deep_learning.dl_manager import classifiers
 
 def test():
     cli.show_combination_strategies()
@@ -14,3 +17,53 @@ def get_cli_json():
     file_path = path.abspath('../../mining-design-decisions/deep_learning/dl_manager/cli.json')
     with open(file_path, 'r') as f:
         return json.load(f)
+
+def get_models_strlist():
+    return list(classifiers.models)
+
+def get_hyper_param_options():
+    cla = classifiers.models
+    results = {}
+
+    for c in cla:
+        results[c] = []
+        classifier = cla[c]
+        for name, param in classifier.get_hyper_parameters().items():
+            results[c].append((f'{name} -- '
+                               f'[min, max] = [{param.minimum}, {param.maximum}] -- '
+                               f'default = {param.default}'))
+
+    return results
+
+def get_input_modes():
+    return list(feature_generators.generators)
+
+def get_input_mode_params():
+    inmodes = feature_generators.generators
+    results = {}
+    for inmode in inmodes:
+        params = inmodes[inmode].get_parameters()
+        results[inmode] = []
+        for name, param in params.items():
+            results[inmode].append(f'{name} -- {param.description}')
+    return results
+
+def get_output_modes():
+    result = []
+    for key in vars(feature_generators.OutputMode):
+        if '_' not in key: # todo this is a small bug that still needs to be solved in the CLI
+            result.append(key)
+    return result
+
+# deprecated for now - maybe for the ontology edit page later
+def get_possible_ontologies():
+    result = []
+    dir = os.fsencode('../../mining-design-decisions/datasets/ontologies')
+    for file in os.listdir(dir):
+        filename = os.fsdecode(file)
+        if filename.endswith('.txt'):
+            result.append(filename[:-4])
+    return result
+
+def get_combination_strategies():
+    return cli.STRATEGIES
