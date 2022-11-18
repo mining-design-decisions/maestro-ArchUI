ada882e700a122ad0c82d33bf597624e16fecec7
Sarah Druyts <sarah.druyts@gmail.com>
2022-11-18 17:37:26 +0100

    Run Models View

--- flask/app/blueprints/run.py
+++ flask/app/blueprints/run.py
@@ -1,12 +1,20 @@
+from flask import render_template
 from flask import Blueprint
 from flask import redirect
 from flask import request
+import os
 
 bp = Blueprint('run', __name__, url_prefix='/run')
 
 @bp.route('/select', methods=['GET'])
 def select():
-    return 'ok'
+    dir = 'app/models'
+    models = []
+    for file in os.listdir(dir):
+        if file.endswith('.json'):
+            models.append(file[:-5])
+
+    return render_template('run/select.html', models=models)
 
 @bp.route('/select', methods=['POST'])
 def postSelect():
