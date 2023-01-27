3ef67a26d3d0b33bdf48d7a02a63b96e85912004
Sarah Druyts <sarah.druyts@gmail.com>
2023-01-27 13:39:56 +0100

    issue classification send data

--- flask/app/blueprints/issues.py
+++ flask/app/blueprints/issues.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import render_template
 import os
 import json
+from flask import request
 
 bp = Blueprint("issues", __name__, url_prefix="/issues")
 
@@ -13,24 +14,13 @@ def viewall():
 
 @bp.route('/view/<list_name>', methods=["GET"])
 def view(list_name):
-    # BIG TODO WITH THE WHOLE TABLE
-    # AND THE PAGINATION
-    issues = { # temp test data
-        "TAJO-3": {
-            "Example Model: Prediction Name": "Architectural",
-            "Example Model: Probability Architectural": "1.00000"
-        },
-        "TAJO-2": {
-            "Example Model: Prediction Name": "Architectural",
-            "Example Model: Probability Architectural": "1.00000"
-        },
-        "TAJO-1": {
-            "Example Model: Prediction Name": "Non-Architectural",
-            "Example Model: Probability Architectural": "0.00000"
-        }
-    }
     with open(f'app/data/runs/{list_name}.json', 'r') as f:
         issues = json.load(f)
     first_issue = list(issues.values())[0]
     headers = list(first_issue.keys())
     return render_template("issues/view.html", issues=issues, headers=headers)
+
+@bp.route('/classify', methods=["POST"])
+def classify():
+    print(request.get_json())
+    return 'ok'
