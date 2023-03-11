792ea78c066443e267b7c5366d9025002424d2d3
Sarah Druyts <sarah.druyts@gmail.com>
2023-03-11 20:32:34 +0100

    [db part 2.2] statistics stub + predict UI
    not yet predict functionality

--- flask/app/blueprints/predict.py
+++ flask/app/blueprints/predict.py
@@ -1,9 +1,16 @@
-from flask import render_template_string
+from flask import render_template
 from flask import Blueprint
 
 bp = Blueprint('predict', __name__, url_prefix="/predict")
 
 @bp.route('/', methods=["GET"])
 def viewform():
+    # todo
+    models = ["testmodel", "model1", "model2"]
+    projects = ["testproj", "test", "proj"]
+    return render_template("predict/form.html", models=models, projects=projects)
+
+@bp.route('/', methods=["POST"])
+def predict():
     # todo
     return 'under construction'
--- flask/app/blueprints/statistics.py
+++ flask/app/blueprints/statistics.py
@@ -1,4 +1,4 @@
-from flask import render_template_string
+from flask import render_template
 from flask import Blueprint
 
 bp = Blueprint('statistics', __name__, url_prefix="/statistics")
@@ -6,4 +6,4 @@ bp = Blueprint('statistics', __name__, url_prefix="/statistics")
 @bp.route('/', methods=["GET"])
 def view():
     # todo
-    return 'under construction'
+    return render_template('statistics/view.html')
