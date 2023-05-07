8c4db3bb9531e10eab239723cf47b906188d700f
Sarah Druyts <sarah.druyts@gmail.com>
2023-05-07 17:22:54 +0200

    model config saving & viewing

--- flask/app/services/modelconfig.py
+++ flask/app/services/modelconfig.py
@@ -1,5 +1,20 @@
+from app.services import dbapi
+
 def get_by_prefix(formdata, prefix):
     return [x for x in formdata if x.startswith(prefix)]
+
+def get_params_by_prefix(formdata, prefix, is_prepro):
+    params = {}
+    for p in get_by_prefix(formdata, prefix):
+        if formdata.get(p) and len(formdata.get(p).strip()) > 0:
+            params[f"{p[len(prefix):].replace('_', '-')}"] = formdata.get(p)
+    if is_prepro and f"{prefix}embedding_id" in formdata:
+        e = dbapi.get_embedding(formdata.get(f"{prefix}embedding_id"))
+        dicname = list(e['config']['params'].keys())[0]
+        for param in e['config']['params'][dicname]:
+            if param not in ['min-count', 'min-doc-count']: # these aren't in the param lists for feature generators
+                params[f"{p[len(prefix):].replace('_', '-')}"] = formdata.get(p)
+    return params
 
 def raw_to_config(formdata):
     config = {}
@@ -8,38 +23,38 @@ def raw_to_config(formdata):
     config['output_mode'] = formdata.get('gen_output_mode', None)
     model_mode = formdata.get('gen_model_mode', 'Single')
 
-    strat = formdata.get('gen_combination_strategy', None)
-    config['ensemble_strategy'] = "none" # default?
-    config['combination_strategy'] = "concat" # default?
-    if strat.lower() in ["stacking", "voting"]:
-        config['ensemble_strategy'] = strat.lower()
-    elif strat:
-        config['combination_strategy'] = strat.lower()
-
-    config['stacking_meta_classifier'] = None
-    config['stacking_meta_classifier_hyper_parameters'] = None
-    config['stacking_use_concat'] = False
-    config['stacking_no_matrix'] = True
-
     if model_mode == 'Single':
         # single mode tabs:
         # pre-processing
         config['input_mode'] = [formdata.get('single_inmode')]
-        p_prefix = "single_p_"
-        config['params'] = []
-        for p in get_by_prefix(formdata, p_prefix):
-            if formdata.get(p) and len(formdata.get(p).strip()) > 0:
-                config['params'].append(f"{p[len(p_prefix):].replace('_', '-')}={formdata.get(p)}")
+        config['params'] = {
+            f"{config['input_mode'][0]}.0": get_params_by_prefix(formdata, "single_p_", True)
+        }
 
         # classifier
         config['classifier'] = [formdata.get('single_classifier')]
-        h_prefix = "single_hp_"
-        config['hyper_params'] = []
-        for hp in get_by_prefix(formdata, h_prefix):
-            if formdata.get(hp) and len(formdata.get(hp).strip()) > 0:
-                config['hyper_params'].append(f"{hp[len(h_prefix):].replace('_', '-')}={formdata.get(hp)}")
+        config['hyper_params'] = {
+            f"{config['classifier'][0]}.0": get_params_by_prefix(formdata, 'single_hp_', False)
+        }
+        if config['classifier'][0] == 'LinearConv1Model':
+            config['analyze_keywords'] = formdata.get('single_analyze_keywords', False) == "on"
 
     else:
+        test_sep = formdata.get('gen_test_separately', False)
+        if test_sep:
+            config['test_separately'] = test_sep
+
+        strat = formdata.get('gen_combination_strategy', None)
+        config['ensemble_strategy'] = "none" # default?
+        config['combination_strategy'] = "concat" # default?
+        if strat.lower() in ["stacking", "voting"]:
+            config['ensemble_strategy'] = strat.lower()
+        elif strat:
+            config['combination_strategy'] = strat.lower()
+
+        if strat.lower() == 'voting':
+            config['voting_mode'] = formdata.get('gen_voting_mode')
+
         # ensemble mode tabs:
         # ensemble classifiers
         class_count = {}
@@ -47,12 +62,12 @@ def raw_to_config(formdata):
         hparams = []
         params = []
 
-        config['hyper_params'] = []
-        config['params'] = []
+        config['hyper_params'] = {}
+        config['params'] = {}
         config['classifier'] = []
         config['input_mode'] = []
 
-        amt = formdata.get('ens_classifier_count')
+        amt = int(formdata.get('ens_classifier_count'))
         for i in range(amt):
             # classifier
             this_class = formdata.get(f"ens_{i}_classifier")
@@ -66,10 +81,7 @@ def raw_to_config(formdata):
             if this_class not in class_count:
                 class_count[this_class] = 0
 
-            hp_pref = f"ens_{i}_hp_"
-            for hp in get_by_prefix(formdata, hp_pref):
-                if formdata.get(hp) and len(formdata.get(hp).strip()) > 0:
-                    config['hyper_params'].append(f"{this_class}[{class_count[this_class]}].{hp[len(hp_pref):].replace('_', '-')}={formdata.get(hp)}")
+            config['hyper_params'][f"{this_class}.{class_count[this_class]}"] = get_params_by_prefix(formdata, f"ens_{i}_hp_", False)
 
             class_count[this_class] += 1
 
@@ -77,23 +89,16 @@ def raw_to_config(formdata):
             if this_inmode not in inmode_count:
                 inmode_count[this_inmode] = 0
 
-            p_pref = "ens_{i}_p_"
-
-            for p in get_by_prefix(formdata, p_pref):
-                if (formdata.get(p)) and len(formdata.get(p).strip()) > 0:
-                    config['params'].append(f"{this_inmode}[{inmode_count[this_inmode]}].{p[len(p_pref):].replace('_', '-')}={formdata.get(p)}")
+            config['params'][f"{this_inmode}.{inmode_count[this_inmode]}"] = get_params_by_prefix(formdata, "ens_{i}_p_", True)
 
             inmode_count[this_inmode] += 1
 
         # if applicable, the stacker
         if config['ensemble_strategy'] == "stacking":
             config['stacking_meta_classifier'] = formdata.get('stacker_classifier', None)
-            prefix = 'stacker_hp_'
-            config['stacking_meta_classifier_hyper_parameters'] = []
-
-            for hp in get_by_prefix(formdata, prefix):
-                if formdata.get(hp) and len(formdata.get(hp).strip()) > 0:
-                    config['stacking_meta_classifier_hyper_parameters'].append(f"{hp[len('stacker_hp_'):].replace('_','-')}={formdata.get(hp)}")
+            config['use_concat'] = formdata.get('stacker_use_concat', False) == 'on'
+            config['no_matrix'] = formdata.get('stacker_no_matrix', False) == 'on'
+            config['stacking_meta_classifier_hyper_parameters'] = get_params_by_prefix(formdata, 'stacker_hp_', False)
 
     # training
     config['ontology_classes'] = "./dl_manager/feature_generators/util/ontologies.json"
@@ -109,15 +114,21 @@ def raw_to_config(formdata):
     if formdata.get('train_batch_size', 32):
         config['batch_size'] = formdata.get('train_batch_size', 32)
     config['boosting_rounds'] = 0 # not used
+    
+    # early stopping
     config['use_early_stopping'] = formdata.get('train_use_early_stopping', False)
     if formdata.get('train_early_stopping_patience', 5):
         config['early_stopping_patience'] = formdata.get('train_early_stopping_patience', 5)
-    config['early_stopping_min_delta'] = []
-    config['early_stopping_attribute'] = []
-    if (formdata.get('train_early_stopping_min_delta', None)):
-        config['early_stopping_min_delta'] = [formdata.get('train_early_stopping_min_delta', None)] # todo input these better
-    if (formdata.get('train_early_stopping_attribute', None)):
-        config['early_stopping_attribute'] = [formdata.get('train_early_stopping_attribute', "loss")]
+    amt_attrib = formdata.get('train_early_stopping_num_attribs', 0)
+    if not amt_attrib:
+        amt_attrib = 0
+    amt_attrib = int(amt_attrib)
+    if amt_attrib > 0:
+        config['early_stopping_min_delta'] = []
+        config['early_stopping_attribute'] = []
+    for i in range(0, amt_attrib):
+        config['early_stopping_min_delta'].append(formdata.get(f"train_early_stopping_min_{i+1}_size"))
+        config['early_stopping_attribute'].append(formdata.get(f"train_early_stopping_{i+1}_size"))
 
     # other assorted parameters
     config['store_model'] = True
@@ -128,9 +139,13 @@ def config_to_display(config):
     # two common tabs for single & ensemble
     general = {
         "output mode": config['output_mode'],
-        "combination strategy": config["combination_strategy"],
-        "ensemble strategy": config["ensemble_strategy"]
     }
+    if 'combination_strategy' in config:
+        general['combination strategy'] = config['combination_strategy']
+    if 'ensemble_strategy' in config:
+        general['ensemble strategy'] = config['ensemble_strategy']
+    if 'voting_mode' in config:
+        general['voting mode'] = config['voting_mode']
 
     train_fields = [
         "ontology_classes",
@@ -151,9 +166,6 @@ def config_to_display(config):
     for field in train_fields:
         if field in config:
             train_key = field.replace('_', ' ')
-            # if type(config[field]) == list:
-            #     training[train_key] = ', '.join(str(config[field]))
-            # else:
             training[train_key] = config[field]
 
     result = {
@@ -168,18 +180,16 @@ def config_to_display(config):
             'hyper-params': {}
         }
         if config['hyper_params']:
-            for hp in config['hyper_params']:
-                split = hp.split('=')
-                classifier['hyper-params'][split[0]] = split[1]
+            classifier['hyper-params'] = config['hyper_params']
+        if classifier['classifier'] == 'LinearConv1Model':
+            general['analyze-keywords'] = config['analyze_keywords']
         
         input_mode = {
             'input-mode': config['input_mode'][0],
             "params": {}
         }
         if config['params']:
-            for p in config['params']:
-                split = p.split('=')
-                input_mode['params'][split[0]] = split[1]
+            input_mode['params'] = config['params']
 
         result['classifier'] = classifier
         result['pre-processing'] = input_mode
@@ -193,29 +203,18 @@ def config_to_display(config):
         for i in range(len(config['classifier'])):
             this_classifier = config['classifier'][i]
             this_inmode = config['input_mode'][i]
-            this_obj = {
-                'classifier': this_classifier,
-                'input-mode': this_inmode,
-                'params': {},
-                'hyper-params': {}
-            }
+            
             if not this_classifier in class_count:
                 class_count[this_classifier] = 0
             if not this_inmode in inmode_count:
                 inmode_count[this_inmode] = 0
-
-            hp_pref = f"{this_classifier}[{class_count[this_classifier]}]."
             
-            if config['hyper_params']:
-                for hp in [x[len(hp_pref):] for x in config['hyper_params'] if x.startswith(hp_pref)]:
-                    split = hp.split('=')
-                    this_obj['hyper-params'][split[0]] = split[1]
-            
-            if config['params']:
-                p_pref = f"{this_inmode}[{inmode_count[this_inmode]}]."
-                for p in [x[len(p_pref):] for x in config['params'] if x.startswith(p_pref)]:
-                    split = p.split('=')
-                    this_obj['params'][split[0]] = split[1]
+            this_obj = {
+                'classifier': this_classifier,
+                'input-mode': this_inmode,
+                'params': config['params'][f"{this_inmode}.{inmode_count[this_inmode]}"],
+                'hyper-params': config['hyper_params'][f"{this_classifier}.{class_count[this_classifier]}"]
+            }
 
             class_count[this_classifier] += 1
             inmode_count[this_inmode] += 1
@@ -226,12 +225,12 @@ def config_to_display(config):
         if config['ensemble_strategy'] == "stacking":
             result['meta classifier'] = {
                 "classifier": config['stacking_meta_classifier'],
-                "hyper-params": {}
+                "use-concat": config['use_concat'],
+                "no-matrix": config['no_matrix'],
+                "hyper-params": {
+                    f"{config['stacking_meta_classifier']}.0": config['stacking_meta_classifier_hyper_parameters']
+                }
             }
-
-            for hp in config['stacking_meta_classifier_hyper_parameters']:
-                split = hp.split('=')
-                result['hyper-params'][split[0]] = split[1]
 
     return result
 
