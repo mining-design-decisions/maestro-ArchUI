3106c37be614d3b9b0cb6847309753873e8535c7
Jesse Maarleveld <jessemaarleveld@gmail.com>
2023-09-26 14:07:34 +0200

    Expand configurability of the search engine

--- flask/app/data/search.py
+++ flask/app/data/search.py
@@ -16,7 +16,32 @@ def get_single_issue_data(issue_id):
     return x.json()["data"][0]
 
 def get_search_data(body):
-    x = requests.get(f"{login_data.get_search()}/search", verify=False, json=body)
+    x = requests.post(f"{login_data.get_search()}/search", verify=False, json=body)
     if not x.status_code == 200:
         print(x.json())
     return x.json()
+
+
+def index_search_data(body):
+    x = requests.post(f'{login_data.get_search()}/create-index', verify=False, json=body)
+    if x.status_code == 200:
+        print(x.json())
+    return x.json()
+
+
+def get_projects_by_repo():
+    r = requests.get(f'{login_data.get_db()}/repos', verify=False)
+    if not r.status_code == 200:
+        print(r.json)
+        return []
+    repos = r.json()['repos']
+    projects_by_repo = {}
+    for repo in repos:
+        r = requests.get(f'{login_data.get_db()}/repos/{repo}/projects', verify=False)
+        if not r.status_code == 200:
+            print(r.json())
+        projects_by_repo[repo] = r.json()['projects']
+    return {
+        'repos': repos,
+        'projects_by_repo': projects_by_repo
+    }
--- flask/app/view/search.py
+++ flask/app/view/search.py
@@ -2,20 +2,33 @@ from flask import render_template
 from flask import Blueprint
 from flask import request
 
+from app.data import login as l_data
 from app.data import search as data
+from app.data.ml import models as m_data
 from app.controller import search as contr
 
 bp = Blueprint('search', __name__, url_prefix="/search")
 
 @bp.route('/', methods=["GET"])
 def view():
-    return render_template('search/view.html')
+    models = m_data.get_model_ids_names()
+    return render_template('search/view.html', models=models, db_url=l_data.get_db())
 
 @bp.route('/search', methods=["POST"])
 def search():
-    return data.get_search_data(request.json)
+    resp =  data.get_search_data(request.json)
+    return resp
+
+@bp.route('/index', methods=['POST'])
+def index():
+    resp = data.index_search_data(request.json)
+    return resp
 
 @bp.route('/issue/<issue_id>', methods=["GET"])
 def view_issue(issue_id):
     data, labels = contr.get_issue_data_label(issue_id)
     return render_template('search/view_issue.html', data=data, labels=labels)
+
+@bp.route('/get_projects_by_repo', methods=['GET'])
+def get_projects_by_repo():
+    return data.get_projects_by_repo()
