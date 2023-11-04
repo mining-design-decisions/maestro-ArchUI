e243b073505179c0a91431e27e6d4fbfb588d529
arjan <arjan@pop-os.localdomain>
2023-11-04 17:18:30 +0100

    Fixed mobile menu

--- src/main_menu.tsx
+++ src/main_menu.tsx
@@ -1,9 +1,8 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import React from "react";
-import { Link } from "react-router-dom";
 
 const navigation = [
   { name: "Classify Issues", href: "/classify-issues", current: false },
@@ -41,11 +40,6 @@ export default function MainMenu() {
                   <a href="/" className="text-xl font-bold text-inherit">
                     Arch UI
                   </a>
-                  {/* <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  /> */}
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
@@ -128,56 +122,36 @@ export default function MainMenu() {
             </div>
           </div>
 
-          {/* <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               <Disclosure.Button
-                key="Test"
-                as="button"
+                key={"Word Embeddings"}
+                as="a"
+                href={"/ml-models/embeddings"}
                 className={classNames(
-                  true
+                  false
                     ? "bg-gray-900 text-white"
                     : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                  "inline-flex rounded-md px-3 py-2 text-base font-medium"
+                  "block rounded-md px-3 py-2 text-base font-medium"
                 )}
-                aria-current={true ? "page" : undefined}
+                aria-current={false ? "page" : undefined}
               >
-                ML Models
-                <ChevronDownIcon
-                  className="-mr-1 h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
+                {"Word Embeddings"}
               </Disclosure.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
+              <Disclosure.Button
+                key={"ML Models"}
+                as="a"
+                href={"/ml-models/ml-models"}
+                className={classNames(
+                  false
+                    ? "bg-gray-900 text-white"
+                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                  "block rounded-md px-3 py-2 text-base font-medium"
+                )}
+                aria-current={false ? "page" : undefined}
               >
-                <Disclosure.Panel className="sm:hidden">
-                  <div className="space-y-1 px-2 pb-3 pt-2">
-                    <Disclosure.Button
-                      key="Test"
-                      as="button"
-                      className={classNames(
-                        true
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "inline-flex rounded-md px-3 py-2 text-base font-medium"
-                      )}
-                      aria-current={true ? "page" : undefined}
-                    >
-                      ML Models
-                      <ChevronDownIcon
-                        className="-mr-1 h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </Disclosure.Button>
-                  </div>
-                </Disclosure.Panel>
-              </Transition>
+                {"ML Models"}
+              </Disclosure.Button>
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
@@ -195,7 +169,7 @@ export default function MainMenu() {
                 </Disclosure.Button>
               ))}
             </div>
-          </Disclosure.Panel> */}
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
