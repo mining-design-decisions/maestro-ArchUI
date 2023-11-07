e83a574c28bac91893782b43e1c76139daa73766
arjan <arjan@pop-os.localdomain>
2023-11-07 14:31:20 +0100

    Refactored tags page

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,13 +3,19 @@ import React from "react";
 interface IButton {
   label: string;
   onClick: () => void;
-  icon: null | React.JSX.Element;
+  icon?: null | React.JSX.Element;
+  color?: string;
 }
 
-export function Button({ label, onClick, icon = null }: IButton) {
+export function Button({
+  label,
+  onClick,
+  icon = null,
+  color = "blue",
+}: IButton) {
   return (
     <button
-      className="flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-lg"
+      className={`flex bg-${color}-500 hover:bg-${color}-700 text-white font-bold py-1 px-2 rounded-lg`}
       onClick={onClick}
     >
       {icon}
--- src/components/forms.tsx
+++ src/components/forms.tsx
@@ -9,7 +9,7 @@ export function TextForm({ label, value, onChange, password = false }) {
         value={value}
         onChange={onChange}
         type={password ? "password" : "text"}
-        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="w-full bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
     </div>
   );
